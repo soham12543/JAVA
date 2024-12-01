348a02d606d349a28b23569b701b87406694625b
soham12543 <sohamofficial801@gmail.com>
2024-12-01 21:27:20 +0530

    For loop added

--- Code1.java
+++ Code1.java
@@ -1,7 +1,13 @@
+
+
 class Soham{
     public static void main(String args[])
     {
         System.out.println("Hello World");
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println("This is a new file");
+        }
 
     }
 }
